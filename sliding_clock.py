e4f96b19d275c9578a8d85a5ec556f1aed283dbc
Andrea Montefusco IW0HDV <andrew@montefusco.com>
2017-01-11 20:01:33 +0100

    sliding_clock fixed

--- sliding_clock.py
+++ sliding_clock.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
-# Sliding digital clock on single 32x32 RGB led panel
+# Sliding digital clock on RGB led panel
+# multiple module capability added by A.Montefusco 2017, 
+# requires ledpanel.ko 2.0
 
 import time
 import sys
@@ -9,19 +11,22 @@ from PIL import ImageFont
 from PIL import Image
 from PIL import ImageDraw
 import StringIO
+import probe
 
 if len(sys.argv)<2 or len(sys.argv)>4:
 	print "Syntax:"
 	print "  %s r g b" % (sys.argv[0])
 	quit()
 
-font = ImageFont.truetype('fonts/Ubuntu-B.ttf', 32)
+print "Panel size: %d x %d\n" % (probe.panel_w, probe.panel_h)
+
+font = ImageFont.truetype('fonts/Ubuntu-B.ttf', probe.panel_h)
 
 r=int(sys.argv[1])
 g=int(sys.argv[2])
 b=int(sys.argv[3])
   
-im = Image.new("RGB", (32*3, 32), "black")
+im = Image.new("RGB", (probe.panel_w, probe.panel_h), "black")
 draw = ImageDraw.Draw(im)
 draw.fontmode="1" #No antialias
 width, height = font.getsize("88:88:88")
@@ -31,7 +36,7 @@ def format_time():
     return "{:%H:%M:%S}".format(d)
  
 message = format_time()
-x = 32*3
+x = probe.panel_w
 
 out_file = open("/sys/class/ledpanel/rgb_buffer","w")
 output = StringIO.StringIO()
@@ -39,11 +44,11 @@ while True:
 	x = x - 1
 
 	if x < -(width):
-		x = 32*3
+		x = probe.panel_w
 		message = format_time()
 		continue
 		
-	draw.rectangle((0, 0, 32*3-1, height), outline=0, fill=0)
+	draw.rectangle((0, 0, probe.panel_w - 1, height), outline=0, fill=0)
 	draw.text((x, -1), message, (r<<5,g<<5,b<<5), font=font)
 
 	output.truncate(0)
